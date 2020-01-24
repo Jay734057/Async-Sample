fdae7e29dece9c3460c163535d2054387d2b8123
jianyu <zjy.au.0817@gmail.com>
2020-01-24 14:25:18 +1100

    initial files

--- src/App.js
+++ src/App.js
@@ -2,25 +2,38 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      number: this.props.init
+    };
+
+    console.log(this.props);
+  }
+
+  onClickHandle = () => {
+    this.setState((preStates, preProps) => {
+    return { number: preStates.number + 1}}, 
+    () => {
+      console.log(this.state.number);
+    } );
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            {this.state.number}
+          </p>
+          <button onClick={this.onClickHandle}>Add</button>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
